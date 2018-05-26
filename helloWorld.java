742b5de3d672611641ca0379b30af365dd660234
Riley Johnson <rileymillerjohnson@gmail.com>
2018-05-25 20:57:54 -0400

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -3,11 +3,7 @@ package spark.examples.hello;
 import static spark.Spark.get;
 
 public class HelloWorld {
-
-	    public static void main(String[] args) {
-
-		            get("/hello", (request, response) -> "Hello World!");
-
-			        }
-
+	public static void main(String[] args) {
+		get("/hello", (request, response) -> "Hello World!");
+	}
 }
