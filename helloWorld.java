befd4b525b5b755f6c4f87d431ea14f5e9a5d09b
Riley Johnson <rileymillerjohnson@gmail.com>
2018-05-25 20:56:15 -0400

    added hello world file

---
+++ helloWorld.java
@@ -0,0 +1,13 @@
+package spark.examples.hello;
+
+import static spark.Spark.get;
+
+public class HelloWorld {
+
+	    public static void main(String[] args) {
+
+		            get("/hello", (request, response) -> "Hello World!");
+
+			        }
+
+}
